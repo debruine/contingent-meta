bab0e479b198f1a1f28f52a3d45a28d9e64c87e8
debruine <debruine@gmail.com>
2019-06-27 10:04:37 +0100

    Fixed and added anova

--- quick_data_sim.R
+++ quick_data_sim.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(afex)
 
 reps <- 100 # number of simulation reps
 stud <- 2   # number of studies per rep
@@ -6,20 +7,29 @@ n <- 20     # number of participants per study
 d <- 0      # effect size
 
 df <- data.frame(
-  rep = rep(1:reps, each = n*stud),
-  study = rep(rep(1:stud, each = n), times = reps),
-  x = rnorm(n*stud*reps),
-  y = rnorm(n*stud*reps, d),
-  mod = sample(c("a", "b"), n*stud*reps, replace = TRUE)
-)
+  rep = rep(1:reps, each = n*stud*2),
+  study = rep(rep(1:stud, each = n*2), times = reps),
+  grp = rep(rep(c("x", "y"), each = n), times = stud*reps),
+  dv = rnorm(n*stud*reps*2),
+  mod = sample(c("a", "b"), n*stud*reps*2, replace = TRUE)
+) %>%
+  mutate(
+    id = row_number(),
+    dv = dv + (d*(grp == "y"))
+  )
+
+ggplot(df, aes(grp, dv)) +
+  geom_boxplot()
 
 analyses <- df %>%
   group_by(rep, study) %>%
   nest() %>%
-  mutate(t = map(data, ~t.test(.$x, .$y) %>% broom::tidy())) %>%
+  mutate(t = map(data, ~t.test(dv~grp, .) %>% broom::tidy())) %>%
   unnest(t, .sep = ".") %>%
-  mutate(w = map(data, ~wilcox.test(.$x, .$y) %>% broom::tidy())) %>%
+  mutate(w = map(data, ~wilcox.test(dv~grp, .) %>% broom::tidy())) %>%
   unnest(w, .sep = ".") %>%
-  mutate(better = ifelse(t.p.value < w.p.value, "t", "w"))
-
-         
+  mutate(a = map(data, ~{
+    a <- suppressMessages(aov_4(dv~grp+mod(1|id), data = .)$aov)
+    broom::tidy(a)[1,]
+  })) %>%
+  unnest(a, .sep = ".")
